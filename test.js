8eed364b9af15e9a5a3486dc1a7d226ae54a3783
TanyaRoot <kofeneformat@gmail.com>
2018-02-27 16:09:42 +0300

    add description

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log("first steps");
+
+// git init : инициализируешь гитхаб
+
+// git status : проверить какие файлы изменины
+// git add * : добавить все файлы
+// git commit -m : "сообщение коммита"
+// git push --set-upstream origin master : запушить на гитхаб
+
+// [remote "origin"]
+// 	url = "https://github.com/TanyaRoot/learnJs.git" : тут ссылка на репу
+// [branch "master"]
+// 	remote = origin
+// 	merge = refs/heads/master
